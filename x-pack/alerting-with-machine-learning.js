89d8626edbdf0d7f98fe9f09310642b06dbab784
Kosho Owa <kosho@elastic.co>
2017-05-29 16:21:02 +0900

    add ml example

---
+++ x-pack/alerting-with-machine-learning.js
@@ -0,0 +1,79 @@
+{
+  "trigger": {
+    "schedule": {
+      "interval": "1m"
+    }
+  },
+    "input": {
+      "chain": {
+        "inputs": [
+        {
+          "first": {
+            "http": {
+              "request": {
+                "scheme": "http",
+                "host": "localhost",
+                "port": 9200,
+                "method": "get",
+                "path": "_xpack/ml/anomaly_detectors/{{ctx.metadata.job_id}}/results/buckets",
+                "params": {},
+                "headers": {},
+                "auth": {
+                  "basic": {
+                    "username": "elastic",
+                    "password": "changeme"
+                  }
+                },
+                "body": "{\"anomaly_score\": {{ctx.metadata.threashold}}, \"start\": \"now-5m\"}"
+              }
+            }
+          }
+        },
+        {
+          "second": {
+            "http": {
+              "request": {
+                "scheme": "http",
+                "host": "localhost"
+                "port": 9200,
+                "method": "get",
+                "path": "_xpack/ml/anomaly_detectors/{{ctx.metadata.job_id}}/results/influencers",
+                "params": {},
+                "headers": {},
+                "auth": {
+                  "basic": {
+                    "username": "elastic",
+                    "password": "changeme"
+                  }
+                },
+                "body": "{\"start\": \"now-5m\"}"
+              }
+            }
+          }
+        }
+        ]
+      }
+    },
+    "condition": {
+      "compare": {
+        "ctx.payload.first.count": {
+          "gt": 0
+        }
+      }
+    },
+    "actions": {
+      "notify-slack": {
+        "throttle_period_in_millis": 30000,
+        "slack": {
+          "message": {
+            "from": "Machine Learning Job",
+            "text": "Anomaly found by {{ctx.payload.first.buckets.0.job_id}} at {{ctx.execution_time}}.\nInfluenced by:{{#ctx.payload.second.influencers}} * {{influencer_field_name}}: {{influencer_field_value}} {{/ctx.payload.second.influencers}}"
+          }
+        }
+      }
+    },
+    "metadata": {
+      "threashold": 70,
+      "job_id": "ML_JOB_ID"
+    }
+}
